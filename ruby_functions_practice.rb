78bab7f01a3e773309a790e0069d15ecabbbc2ba
hannahgreen1 <hannahgreen94@icloud.com>
2018-04-10 16:21:01 +0100

    pair programming examples

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,35 @@
 def return_10()
 return 10
 end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  length_of_string = string.length
+  return length_of_string
+end
+
+def join_string(string_1, string_2 )
+  joined_string = string_1 + string_2
+  return joined_string
+end
+
+def add_string_as_number(number_1, number_2)
+  add_string_as_number = number_1 + number_2
+return add_string_as_number
+end
